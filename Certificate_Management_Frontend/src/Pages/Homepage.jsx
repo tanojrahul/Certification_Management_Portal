a17b474642b82d1a25a0ed378cf1f3f340a3b45a
tanojrahul <tanojrahul19@gmail.com>
2026-01-30 08:33:59 +0530

    Refactor Login component: move to Pages directory, update import paths, and add new styles

--- Certificate_Management_Frontend/src/Pages/Homepage.jsx
+++ Certificate_Management_Frontend/src/Pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Login from '../components/login';
+import Login from './Login';
 
 function Homepage() {
     
